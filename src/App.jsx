5d819b6f4ae54a0c90bd5172195efdcbfe7b722a
kapishan-2005 <kapishan.official@gmail.com>
2026-01-06 11:41:24 +0530

    eroroCorrect

--- src/App.jsx
+++ src/App.jsx
@@ -23,7 +23,7 @@ useEffect(() => {
         React Application with GitHub API
       </h1>
 
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-4">
+<div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-4">
         {users.map((user) => (
           <div
             key={user.id}
